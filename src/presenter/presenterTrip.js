0cff12253dfbb69a699f983e2adf2f6222dcd895
Яна <leksa87@inbox.ru>
2026-05-09 18:08:01 +0300

    поправила ошибки по линтеру

--- src/presenter/presenterTrip.js
+++ src/presenter/presenterTrip.js
@@ -1,9 +1,9 @@
-import { render, RenderPosition } from "../render.js";
-import ViewFilters from "../view/filters.js";
-import ViewSort from "../view/sort.js";
-import ViewCreatePoint from "../view/create-point.js";
-import ViewEditPoint from "../view/edit-point.js";
-import ViewPoint from "../view/point.js";
+import { render, RenderPosition } from '../render.js';
+import ViewFilters from '../view/filters.js';
+import ViewSort from '../view/sort.js';
+import ViewCreatePoint from '../view/create-point.js';
+import ViewEditPoint from '../view/edit-point.js';
+import ViewPoint from '../view/point.js';
 
 export default class TripPresenter {
   constructor({ filtersContainer, sortContainer, eventsContainer }) {
